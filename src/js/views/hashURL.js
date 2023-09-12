7fe0c8dec34e3021802a79f8577e0cc48808d895
roblittlefield <github.0502@roblittlefield.com>
2023-09-11 19:08:51 -0700

    hasURL cad_number fn arg

--- src/js/views/hashURL.js
+++ src/js/views/hashURL.js
@@ -1,5 +1,5 @@
 const getURLParameter = function (cad_number) {
   const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get(cad);
+  return urlParams.get(cad_number);
 };
 export default getURLParameter;
