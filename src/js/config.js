d91ea9c26fb3dc68f6c2f92950016370cb29ea80
roblittlefield <github.0502@roblittlefield.com>
2023-10-14 16:54:55 -0700

    Stamen reinstate

--- src/js/config.js
+++ src/js/config.js
@@ -23,7 +23,11 @@ export const nearbyCircleOpt = {
 };
 
 export const MAP_LAYERS = [
-  "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
+  // "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
+  // "https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg",
+  // "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+  // "https://tiles.stadiamaps.com/data/satellite/{z}/{x}/{y}.jpg",
+  // "https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png",
   "https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg",
   "https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg",
   "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png",
