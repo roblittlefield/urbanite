2ecdce469b599e1de3ff54e72b51fc43e24c797a
roblittlefield <github.0502@roblittlefield.com>
2023-11-11 02:29:13 -0800

    move social buttons to right

--- src/js/views/circleMarkers.js
+++ src/js/views/circleMarkers.js
@@ -69,12 +69,12 @@ export function addCircleMarkers(data, callsLayer, respCircleLayer) {
     \u2022 ${receivedTimeAgoF} <a href="sms:&body=${encodeURIComponent(
       textMessageContent
     )}">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/IMessage_logo.svg/20px-IMessage_logo.svg.png" alt="iMessage / text" style=" height:20px; position: absolute; bottom: 0px; left: calc(50% - 27px); transform: translate(-50%, -50%);">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/IMessage_logo.svg/20px-IMessage_logo.svg.png" alt="iMessage / text" style="height: 20px; position: absolute; top: calc(50% - 25px); right: -10px;">
     </a>
     <a href="https://twitter.com/intent/tweet?text=${encodeURIComponent(
       tweetContent
     )}" target="_blank">
-    <img src="https://icons.iconarchive.com/icons/xenatt/the-circle/256/App-Twitter-icon.png" alt="Twitter Bird Icon" style=" height: 20px; position: absolute; bottom: 0px; left: calc(50% + 25px); transform: translate(-50%, -50%);">
+    <img src="https://icons.iconarchive.com/icons/xenatt/the-circle/256/App-Twitter-icon.png" alt="Twitter Bird Icon" style="height: 24px; position: absolute; top: calc(50% - -5px);  right: -12px;">
     </a>
     <br>${call.properCaseAddress}
     <br>Priority ${
