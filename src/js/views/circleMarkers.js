6c9d9e352597c15aafeef27470ec8b133ef74bf5
roblittlefield <github.0502@roblittlefield.com>
2023-11-18 19:36:37 -0800

    share link classes

--- src/js/views/circleMarkers.js
+++ src/js/views/circleMarkers.js
@@ -72,12 +72,12 @@ export function addCircleMarkers(data, callsLayer, respCircleLayer) {
     \u2022 ${receivedTimeAgoF} <a href="sms:&body=${encodeURIComponent(
       textMessageContent
     )}">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/IMessage_logo.svg/20px-IMessage_logo.svg.png" alt="iMessage / text" style="height: 20px; position: absolute; top: calc(50% - 24px); right: -6px;">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/IMessage_logo.svg/20px-IMessage_logo.svg.png" alt="iMessage / text" style="height: 20px; position: absolute; top: calc(50% - 24px); right: -6px;" class="text-message-btn">
     </a>
     <a href="https://twitter.com/intent/tweet?text=${encodeURIComponent(
       tweetContent
     )}" target="_blank">
-    <img src="https://icons.iconarchive.com/icons/xenatt/the-circle/256/App-Twitter-icon.png" alt="Twitter Bird Icon" style="height: 24px; position: absolute; top: calc(50% - -5px);  right: -8px;">
+    <img src="https://icons.iconarchive.com/icons/xenatt/the-circle/256/App-Twitter-icon.png" alt="Twitter Bird Icon" style="height: 24px; position: absolute; top: calc(50% - -5px);  right: -8px;" class="twitter-btn">
     </a>
     <br>${call.properCaseAddress}
     <br>Priority ${
