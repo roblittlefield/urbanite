015fe0a2e15d3c6d24f4a74d905aef9862faad1c
roblittlefield <github.0502@roblittlefield.com>
2023-11-22 19:07:30 -0800

    add stanley aff

--- src/js/controller.js
+++ src/js/controller.js
@@ -255,11 +255,11 @@ const controlCircleMarkers = async function () {
     }
 
     // Affiliate Link loading
-    let randAffNum = Math.floor(Math.random() * 5);
+    let randAffNum = Math.floor(Math.random() * 6);
     const getAff = function () {
-      randAffNum = (randAffNum + 1) % 5;
+      randAffNum = (randAffNum + 1) % 6;
       return document.querySelectorAll(
-        ".affiliate-1, .affiliate-2, .affiliate-3, .affiliate-4, .affiliate-5"
+        ".affiliate-1, .affiliate-2, .affiliate-3, .affiliate-4, .affiliate-5, .affiliate-6"
       )[randAffNum];
     };
     let affEl = getAff();
