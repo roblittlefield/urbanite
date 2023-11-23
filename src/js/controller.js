0411eefb65c88302168f4c011dae7b75cd47d521
roblittlefield <github.0502@roblittlefield.com>
2023-11-22 18:20:41 -0800

    affiliate links

--- src/js/controller.js
+++ src/js/controller.js
@@ -254,6 +254,51 @@ const controlCircleMarkers = async function () {
       openPopup();
     }
 
+    // Affiliate Link loading
+    let randAffNum = Math.floor(Math.random() * 5);
+    const getAff = function () {
+      randAffNum = (randAffNum + 1) % 5;
+      return document.querySelectorAll(
+        ".affiliate-1, .affiliate-2, .affiliate-3, .affiliate-4, .affiliate-5"
+      )[randAffNum];
+    };
+    let affEl = getAff();
+    affEl.classList.remove("hidden");
+
+    (function affHide() {
+      setTimeout(() => {
+        affEl.classList.add("aff-hidden");
+        affEl.classList.remove("aff-visible");
+        document
+          .getElementsByClassName("affiliate-disclaimer")[0]
+          .classList.add("aff-hidden");
+        document
+          .getElementsByClassName("affiliate-disclaimer")[0]
+          .classList.remove("aff-visible");
+
+        setTimeout(() => {
+          affEl.classList.add("hidden");
+          affEl.classList.remove("aff-hidden");
+          affEl = getAff();
+          affEl.style.opacity = "0";
+          setTimeout(() => {
+            affEl.classList.remove("hidden");
+            setTimeout(() => {
+              affEl.classList.add("aff-visible");
+            }, 100);
+          }, 100);
+          document
+            .getElementsByClassName("affiliate-disclaimer")[0]
+            .classList.add("aff-visible");
+          document
+            .getElementsByClassName("affiliate-disclaimer")[0]
+            .classList.remove("aff-hidden");
+        }, 3700);
+
+        affHide();
+      }, 1000 * 20);
+    })();
+
     // If user location is known, update the nearby calls list
     if (initLoaded && position) loadNearbyCalls();
 
