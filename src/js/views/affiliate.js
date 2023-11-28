c8a56a374539c720d515c980deff9bdee07fa070
roblittlefield <github.0502@roblittlefield.com>
2023-11-27 19:21:50 -0800

    hats hats hats

--- src/js/views/affiliate.js
+++ src/js/views/affiliate.js
@@ -7,8 +7,8 @@ const loadAff = function () {
   })();
 
   // Affiliate Link loading
-  let randAffNum = Math.floor(Math.random() * 6);
-  // let randAffNum = 3;
+  // let randAffNum = Math.floor(Math.random() * 6);
+  let randAffNum = 1;
   let affEls = "";
   affEls = document.querySelectorAll(
     ".affiliate-1, .affiliate-2, .affiliate-3, .affiliate-4, .affiliate-5, .affiliate-6"
