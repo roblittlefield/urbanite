c4e7b81cf520d10a412785941140982ffcab1c4b
roblittlefield <github.0502@roblittlefield.com>
2023-11-29 22:10:59 -0800

    casio

--- src/js/views/affiliate.js
+++ src/js/views/affiliate.js
@@ -12,7 +12,7 @@ const loadAff = function () {
 
   // Affiliate Link loading
   let randAffNum = Math.floor(Math.random() * 6);
-  // let randAffNum = 1;
+  // let randAffNum = 5;
   let affEls = "";
   affEls = document.querySelectorAll(
     ".affiliate-1, .affiliate-2, .affiliate-3, .affiliate-4, .affiliate-5, .affiliate-6"
