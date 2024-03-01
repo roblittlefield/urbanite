863f547fc41659d30693d14d663bd42b09921f37
roblittlefield <github.0502@roblittlefield.com>
2024-02-29 17:41:54 -0800

    X, text 15px higher, left-align responding popup

--- src/js/views/circleMarkers.js
+++ src/js/views/circleMarkers.js
@@ -266,7 +266,6 @@ export function addCircleMarkers(data, callsLayer, respCircleLayer) {
           closeButton: false,
           disableAnimation: true,
           autoPan: false,
-          className: "station-popup",
         })
         .addTo(callsLayer);
       marker.on("click", function (e) {
